b98a6e0fc977431fcd79755c5bb760e7bc7ecfa3
admin <dominique.bertin@geonosis.fr>
2020-03-11 18:48:34 +0100

    Comments

--- sql/cre_table.sql
+++ sql/cre_table.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE tst
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
+-- Création de la base de données
+CREATE DATABASE tst CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Création de l'utilisateur tst
 CREATE USER 'tst'@'localhost' IDENTIFIED BY 'tst';
+
+-- Authorisations
 GRANT ALL PRIVILEGES ON tst.* TO 'tst'@'localhost';
+
+-- Création d ela table des utilisateurs
 CREATE TABLE tst.utilisateurs (
   id int(11) NOT NULL AUTO_INCREMENT,
   nom varchar(20) NOT NULL,
